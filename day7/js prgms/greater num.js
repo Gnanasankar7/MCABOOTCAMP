1cac639427ebf620e11d0ab902397befea48bb72
Gnanasankar7 <mssankar2111@gmail.com>
2025-08-13 10:47:18 +0530

    js

---
+++ day7/js prgms/greater num.js
@@ -0,0 +1,9 @@
+let arr = [10, 20, 30, 45, 45, 30, 45, 78]
+let search = 45;
+let a;
+
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] == search) {
+        a = i
+    }
+    console.log("The index is", a);
